313f954bc163f37ca6afa010366b0d28d44faebd
Michael Shafer <michael.shafer@nau.edu>
2024-04-26 12:59:51 -0700

    added shell script for archiving and resetting the recordings files

---
+++ resetRecorder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#use sudo chmod +x /home/dasl/repos/boring_recorder/resetRecorder.sh
+#in terminal to make this file executable. Otherwise you'll get
+#a permission denied error.
+
+#Zip up all the files
+zipName=$(date +/home/dasl/Desktop/%Y_%m_%d-%H_%M_%S.zip)
+zip $zipName /home/dasl/Desktop/Recordings/*
+zip -ur $zipName /home/dasl/repos/boring_recorder/mycrontablog.txt
+#Trash the old files and clear out the log file 
+gio trash /home/dasl/Desktop/Recordings/* 
+echo "" > /home/dasl/repos/boring_recorder/mycrontablog.txt
